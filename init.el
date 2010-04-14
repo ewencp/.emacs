b1c8e3d04d2fed79934d94eaa5b519c27b246b35
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2010-04-13 22:19:39 -0700

    Much better javascript mode using js2 for highlighting and espresso for indentation.

--- init.el
+++ init.el
@@ -39,7 +39,8 @@
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 (autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
 (autoload 'cmake-mode "cmake-mode" "Major mode for editing CMake build scripts." t)
-(autoload 'javascript-mode "javascript" nil t)
+(autoload 'js2-mode "js2" nil t)
+(autoload 'espresso-mode "espresso-mode" nil t)
 
 ; Set default mode for new buffers to text
 (setq default-major-mode 'text-mode)
@@ -80,7 +81,7 @@
 ; Global color and fonts
 (setq default-frame-alist
       '(
-        (font             .  "8x13")
+        (font             .  "Monospace-10")
         (width            .     156 )
         (height           .     50 )
         (mouse-color      . "White")
@@ -133,7 +134,8 @@
         '("CMakeLists\\.txt\\'" . cmake-mode)
         '("\\.cmake\\'"   . cmake-mode)
         '("\\.doc\\'"     . c++-mode) ; Doxygen docs use C++ comments
-        '("\\.js\\'"     . javascript-mode)
+        '("\\.js\\'"      . js2-mode)
+        '("\\.json\\'"      . js2-mode)
         )
        auto-mode-alist))
 
@@ -238,9 +240,65 @@
   '(lambda ()
      (setq asm-comment-char ?\#)))
 
-(defconst javascript-mode-hook
-  '(lambda ()
-     (run-hooks 'c-mode-hook)))
+; Javascript stuff
+
+; sourced from mihai.bazon.net/projects/editing-javascript-with-emacs-js2-mode
+(defun js2-indent-function ()
+  (interactive)
+  (save-restriction
+    (widen)
+    (let* ((inhibit-point-motion-hooks t)
+           (parse-status (save-excursion (syntax-ppss (point-at-bol))))
+           (offset (- (current-column) (current-indentation)))
+           (indentation (espresso--proper-indentation parse-status))
+           node)
+
+      (save-excursion
+
+        ;; I like to indent case and labels to half of the tab width
+        (back-to-indentation)
+        (if (looking-at "case\\s-")
+            (setq indentation (+ indentation (/ espresso-indent-level 2))))
+
+        ;; consecutive declarations in a var statement are nice if
+        ;; properly aligned, i.e:
+        ;;
+        ;; var foo = "bar",
+        ;;     bar = "foo";
+        (setq node (js2-node-at-point))
+        (when (and node
+                   (= js2-NAME (js2-node-type node))
+                   (= js2-VAR (js2-node-type (js2-node-parent node))))
+          (setq indentation (+ 4 indentation))))
+
+      (indent-line-to indentation)
+      (when (> offset 0) (forward-char offset)))))
+
+(defun js2-mode-hook ()
+  (require 'espresso)
+  (auto-fill-mode t)
+  (defconst fill-column 80)
+  (setq espresso-indent-level 8
+        indent-tabs-mode nil
+        c-basic-offset 8)
+  (c-toggle-auto-state 0)
+  (c-toggle-hungry-state 1)
+  (set (make-local-variable 'indent-line-function) 'my-js2-indent-function)
+  (define-key js2-mode-map [(meta control |)] 'cperl-lineup)
+  (define-key js2-mode-map [(meta control \;)]
+    '(lambda()
+       (interactive)
+       (insert "/* -----[ ")
+       (save-excursion
+         (insert " ]----- */"))
+       ))
+  (define-key js2-mode-map [(return)] 'newline-and-indent)
+  (define-key js2-mode-map [(backspace)] 'c-electric-backspace)
+  (define-key js2-mode-map [(control d)] 'c-electric-delete-forward)
+  (if (featurep 'js2-highlight-vars)
+    (js2-highlight-vars-mode))
+  (message "My JS2 hook"))
+(add-hook 'js2-mode-hook 'my-js2-mode-hook)
 
 ;#####################################################################
 ; extra binds

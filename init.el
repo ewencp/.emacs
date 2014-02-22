eb4dfdc2ec33c9b02e55b2d747ea4d4cf3ae5f90
Ewen Cheslack-Postava <me@ewencp.org>
2014-02-21 19:12:47 -0800

    Add scala mode.

--- init.el
+++ init.el
@@ -368,3 +368,11 @@
 
 ; Copy-and-paste logging
 ;(require 'keylogger)
+
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+(unless (package-installed-p 'scala-mode2)
+  (package-refresh-contents) (package-install 'scala-mode2))
